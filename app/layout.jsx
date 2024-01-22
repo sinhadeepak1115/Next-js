79296a02550735a6d852af15eb27106b56bc2057
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-01-23 02:16:43 +0530

    latest change in project.

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import "@styles/globals.css";
 import Nav from '@components/Nav'
+import Provider from "@components/Provider";
 
 export const metadata = {
   title: "Promptopia",
@@ -9,13 +10,15 @@ const Rootlayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <div className="main">
-          <div className="gradient" />
-        </div>
-        <main className="app">
-          <Nav />
-          {children}
-        </main>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
   )
